a3ac44cbd061bd8ede4863dfa7cfa5b958d001b0
Ricardo Alcantara <alcantararicardo04@gmail.com>
2023-10-21 18:56:28 -0500

    agregando metodo insertar

--- src/main/java/parcial/isil/parcial/Controlador/UserController.java
+++ src/main/java/parcial/isil/parcial/Controlador/UserController.java
@@ -3,11 +3,17 @@ package parcial.isil.parcial.Controlador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import parcial.isil.parcial.Modelo.User;
 import parcial.isil.parcial.Reposotorio.UserRepository;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -23,5 +29,25 @@ public class UserController {
     @GetMapping("/registro")
     public String UserIndex() {
       return "registro";
+    }
+
+    @PostMapping("/crear")
+    public String crearUsuario(
+            RedirectAttributes redirectAttributes,
+            @Valid @ModelAttribute User user,
+            BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "ingresarEstudiante";
+        }
+
+        try {
+            userRepository.save(user);
+            redirectAttributes.addFlashAttribute("exito", true);
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", true);
+            e.printStackTrace();
+        }
+        return "redirect:/Ingreso";
     }
 }
